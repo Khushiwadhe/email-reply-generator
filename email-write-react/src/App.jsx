f5dbef6c6268b5adb97d51edea7130d902976611
Khushi Wadhe <143949814+Khushiwadhe@users.noreply.github.com>
2026-01-15 20:12:44 +0530

    Update App.jsx

--- email-write-react/src/App.jsx
+++ email-write-react/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setLoading(true);
     setError('');
     try{
-      const response = await axios.post("https://email-reply-generator-idxg.onrender.com/api/email/generate",{
+      const response = await axios.post("http://localhost:8080/api/email/generate",{
         emailContent,
         tone,
       });
